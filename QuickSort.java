275c1705a42fb053429e9ab143df9e3d0794c030
Gabi-30-IT <gabitharan1@gmail.com>
2025-11-06 00:51:55 +0530

    Create QuickSort.java

---
+++ QuickSort.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public static Quicksort {
+    private static int steps = 0;
+
+    public static SortResult sort (int[] data) {
+        int[] arr = Arrays.copyOf(data, data.length);
+        steps = 0;
+        long start = System.currentTimeMillis();
+        quicksort(arr, low:0, arr.length - 1);
+        long end = System.currentTimeMillis();
+        System.arraycopy(arr, srcPos: 0, data, destPos: 0, data.length);
+        return new SortResult(end - start, steps);
+    }
+
+    private static void quicksort(int[] arr, int low, int high) {
+        if (low < high) {
+            steps++;
+            int pi = partition (arr, low, high);
+            quicksort(arr, low, pi - 1);
+            quicksort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high){
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            steps++;
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i +1;
+    }
+}
