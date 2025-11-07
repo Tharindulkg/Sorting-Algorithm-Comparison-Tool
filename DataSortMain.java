7d6200aa428dd829383c5ed58fa7f1ea8b099692
Tharindulkg <152068351+Tharindulkg@users.noreply.github.com>
2025-11-07 11:40:28 +0530

    Create DataSortMain.java

---
+++ DataSortMain.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class DataSortMain{
+    private static final Map<String, SortResult> results = new LinkedHashMap<>();
+    private static final Scanner sc = new Scanner (System.in);
+    private static int[] data = new int[0];
+
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("\n----Data Sorter : Sorting Algorithm Comparison Tool ----");
+            System.out.println("1. Enter numbers manually ");
+            System.out.println("2. Generate random numbers ");
+            System.out.println("3. Perform ubble Sort");
+            
+        }
+    }
+}
